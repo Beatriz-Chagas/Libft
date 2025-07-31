112706e0a08598af0094b84656b966f88f609085
Beatriz Chagas De Sousa <bchagas-@c1r3p1.42sp.org.br>
2025-07-31 02:31:23 -0300

    ft_strrchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bchagas- <bchagas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 16:52:13 by bchagas-          #+#    #+#             */
-/*   Updated: 2025/07/29 17:57:39 by bchagas-         ###   ########.fr       */
+/*   Updated: 2025/07/31 01:52:45 by bchagas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,23 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	unsigned char	ch;
+	unsigned char	i;
 
-	ch = (unsigned char)c;
+	i = (unsigned char)c;
 	while (*s)
 	{
 		if (*s == c)
 			return ((char *)s);
 		s++;
 	}
-	if (ch == '\0')
+	if (i == '\0')
 		return ((char *)s);
 	return (NULL);
 }
 // #include <stdio.h>
-// #include <string.h>
 
 // int main() {
 //   char minhaString[] = "Hello, world!";
-//   char *ponteiro;
-
-//   ponteiro = strchr(minhaString, 'w');
-
-//   if (ponteiro != NULL) {
-//     printf("Caractere 'w' encontrado na posição:
-// %ld\n", ponteiro - minhaString);
-//   } else {
-//     printf("Caractere 'w' não encontrado.\n");
-//   }
-
+//     printf("%s", ft_strchr(minhaString, 'l'));
 //   return 0;
 // }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: bchagas- <bchagas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 17:59:22 by bchagas-          #+#    #+#             */
-/*   Updated: 2025/07/29 17:59:34 by bchagas-         ###   ########.fr       */
+/*   Updated: 2025/07/31 02:04:58 by bchagas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	
+	unsigned char	b;
+	int	i;
+
+	i = 0;
+	b = (unsigned char)c;
+	while (s[i])
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		if (s[i] == b)
+			return ((char *)&s[i]);
+		i--;
+	}
+	if (b == '\0')
+		return ((char *)s);
+	return (NULL);
 }
+// #include <stdio.h>
+
+// int main() {
+//   char minhaString[] = "Hello, world!";
+//     printf("%s", ft_strrchr(minhaString, 'e'));
+//   return 0;
+// }
