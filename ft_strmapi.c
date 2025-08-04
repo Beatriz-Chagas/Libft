d7117fa78327c1c60f8de95072f9f397365089ae
Beatriz Chagas De Sousa <bchagas-@c3r2p5.42sp.org.br>
2025-08-04 17:58:55 -0300

    strmapi finalizado...

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: bchagas- <bchagas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 15:23:49 by bchagas-          #+#    #+#             */
-/*   Updated: 2025/08/02 15:33:05 by bchagas-         ###   ########.fr       */
+/*   Updated: 2025/08/04 17:54:01 by bchagas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,41 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	unsigned int	i;
+	char			*nstr;
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	nstr = (char *)malloc(ft_strlen(s) + 1);
+	if (!nstr)
+		return (NULL);
+	while (s[i])
+	{
+		nstr[i] = f(i, s[i]);
+		i++;
+	}
+	nstr[i] = '\0';
+	return (nstr);
 }
+
+// #include <stdio.h>
+
+// char altera(unsigned int i, char c)
+// {
+// 	if (i % 2 == 0 && c >= 'a' && c <= 'z')
+// 		return (c - 32);  // maiúscula
+// 	else
+// 		return (c);
+// }
+
+// int main(void)
+// {
+// 	char *resultado = ft_strmapi("banana", altera);
+// 	if (resultado)
+// 	{
+// 		printf("Resultado: %s\n", resultado);  
+// 		free(resultado); // não esquecer!
+// 	}
+// 	return 0;
+// }
